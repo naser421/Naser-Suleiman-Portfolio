1a04098e626b32eef1c674d9ef0ce9b23996fc35
naser421 <naser421@gmail.com>
2023-03-15 04:50:12 +0300

    Added github and deployment icons

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,8 +11,8 @@ function ProjectCards(props) {
         <div className="content">
               <p className="card-title">{props.name}</p>
               <p class="proj-icons-container">
-              <a href={props.github}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub Repo" /></a> 
-              <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Live Application" /></a> 
+                    <a href={props.github}><img class="project-icon" src="https://img.icons8.com/color/48/000000/github--v1.png" alt="GitHub Repo" /></a> 
+                    <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/color/48/000000/monitor.png" alt="Live Application" /></a> 
        
              
              </p>

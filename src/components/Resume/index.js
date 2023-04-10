3b9904c531fda7797d48be847a767f42facb6a90
naser421 <naser421@gmail.com>
2023-04-10 23:33:37 +0300

    Various code changes

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -9,15 +9,16 @@ function Resume() {
         <div class="mt-5">
           <ul>
             <li>
-                I am an aspiring Frontend and UI developer looking to work on challenging JavaScript based applications.
+                          As a certified Full Stack Developer from the UC Berkeley Coding Bootcamp, I am excited to bring my skills and experience into this new field.
+
+                          My passion for healthcare and technology led me to my current position as a Lead Bronchoscopy Technician at UCSF Medical Center, where I have gained valuable experience in managing billing, developing protocols, and training new technicians. Additionally, my experience as a Customer Service Representative at MilkManSF and Emergency Medical Technician at Mobility Express Medical Transport has given me the skills to excel in a customer-facing role, as well as the ability to work effectively under pressure. I am confident in my ability to leverage my technical skills in SQL, Node.js, and data analysis to support your mission of providing client solutions. My proficiency in Epic EMR software and EndoPro by Pentax for billing further enhances my ability to contribute to the team.
+
+                          As a natural problem solver with excellent communication skills, I believe I would be an asset to any team. Please take a look at my resume and thank you for considering my application. I look forward to the opportunity to further discuss my qualifications and enthusiasm!
             </li>
           </ul>
 
-         <a
-                      href="https://github.com/naser421/React-Portfolio-Challenge/files/10975130/Naser_Resume.pdf"
-            class="link"
-          >
-            Download My Resume 
+         <a href="https://github.com/naser421/React-Portfolio-Challenge/files/10975130/Naser_Resume.pdf"
+            class="link" > Download My Resume 
           </a>
         </div>
       </div>
@@ -28,7 +29,9 @@ function Resume() {
           <ul>
             <li>
                           As a full stack developer trained in the Full Stack Development Coding Bootcamp, I have mastered the following technologies:
-                          HTML, CSS, JavaScript, Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose and React
+                          HTML, CSS, JavaScript, Node.js, Express.js, <a href="https://user-images.githubusercontent.com/112986806/216163491-2fe2ac3b-a808-48bc-bbc7-49abd6677227.webm"
+                              class="link" > MySQL
+                          </a>, Sequelize, MongoDB, Mongoose and React
             </li>
           </ul>
         </div>
